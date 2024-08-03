03ec3162d9534e130b2004a573d8615396b4cac4
hojimurod07 <hojimurad2007@gmail.com>
2024-08-03 11:43:50 +0500

    problem added

---
+++ problem17.py
@@ -0,0 +1,23 @@
+
+matn = "Salom bollar ishlar qalay ? Hali imtihon tugamadimi? "
+
+
+start= 0
+end = 0
+i = 0
+count = 0
+while True:
+    if matn[i] =='h' or matn[i]=='H' and count==0:
+        start = i
+        count+=1
+    elif matn[i] =='h' or matn[i]=='H'  and count==1:
+        end = i
+        break
+    i+=1
+
+print(matn[start+1:end])
+
+
+natija =[]
+natija.append(matn)
+natija.append(matn[start+1:end]*2)
